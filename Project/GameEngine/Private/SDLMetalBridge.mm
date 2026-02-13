6bb03749b0e1f42ff7c5f041cbec237f3d022b6f
NorbertGerberg <161964914+NorbertGerberg@users.noreply.github.com>
2026-02-13 22:28:16 +0100

    ddd

--- Project/GameEngine/Private/SDLMetalBridge.mm
+++ Project/GameEngine/Private/SDLMetalBridge.mm
@@ -3,11 +3,11 @@
 	Created by Norbert Gerberg.
 ======================================================*/
 #import <Cocoa/Cocoa.h>
-#import <SDL3/SDL_syswm.h>
+#import <SDL3/SDL_system.h>
 
 extern "C" void* GetMetalLayerFromSDL(SDL_PropertiesID* wid)
 {
-	NSWindow* nswindow = (NSWindow*)SDL_GetPointerProperty(&wid, SDL_PROP_WINDOW_COCOA_WINDOW_POINTER, nullptr);
+	NSWindow* nswindow = (NSWindow*)SDL_GetPointerProperty(*wid, SDL_PROP_WINDOW_COCOA_WINDOW_POINTER, nullptr);
 	NSView* contentView = [nswindow contentView];
 	return contentView.layer;
 }
