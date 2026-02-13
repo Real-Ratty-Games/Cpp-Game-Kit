31059fe7e0c43bd2c3e69ec822bfd36f2c544a4a
NorbertGerberg <161964914+NorbertGerberg@users.noreply.github.com>
2026-02-13 19:47:41 +0100

    Update Window.cpp

--- Project/GameEngine/Source/Window.cpp
+++ Project/GameEngine/Source/Window.cpp
@@ -155,5 +155,12 @@ bool Window::GetFullscreen()
 void* Window::GetNativePtr()
 {
 	SDL_PropertiesID wid = SDL_GetWindowProperties(mWndHandle);
-	return SDL_GetPointerProperty(wid, SDL_PROP_WINDOW_WIN32_HWND_POINTER, nullptr);
+#if WIN32
+	void* hwnd = SDL_GetPointerProperty(wid, SDL_PROP_WINDOW_WIN32_HWND_POINTER, nullptr);
+#elif APPLE
+	void* hwnd = SDL_GetPointerProperty(wid, SDL_PROP_WINDOW_COCOA_WINDOW_POINTER, nullptr);
+#else
+	void* hwnd = SDL_GetPointerProperty(wid, SDL_PROP_WINDOW_X11_WINDOW_NUMBER, nullptr);
+#endif
+	return hwnd;
 }
